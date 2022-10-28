a2008efbba803c0907b4f955133ee89ce1033a50
xerophytaxoulis <suselphoenix@gmx.de>
2022-10-28 18:19:06 +0200

    added lombok to the project

--- src/main/java/com/example/payroll/model/Employee.java
+++ src/main/java/com/example/payroll/model/Employee.java
@@ -3,70 +3,38 @@ package com.example.payroll.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Objects;
 
 @Entity
+@NoArgsConstructor
+@Data
 public class Employee {
     @Id
     @GeneratedValue
     private Long Id;
 
-    private String name;
+    private String firstName;
+
+    private String lastName;
 
     private String role;
 
-    public Employee() {
-    }
-
-    public Employee(String name, String role) {
-        this.name = name;
+    public Employee(String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
-    }
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
     }
 
     public String getName() {
-        return name;
+        return String.join(" ", firstName, lastName);
     }
 
     public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return Objects.equals(Id, employee.Id) && Objects.equals(name, employee.name) && Objects.equals(role, employee.role);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(Id, name, role);
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "Id=" + Id +
-                ", name='" + name + '\'' +
-                ", role='" + role + '\'' +
-                '}';
+        String[] nameParts = name.split(" ");
+        firstName = nameParts[0];
+        lastName = nameParts[1];
     }
 }
